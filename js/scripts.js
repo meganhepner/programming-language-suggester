ec02083499b39ca750e8ef900cbcc70399e227e2
Megan Hepner <megan.hepner@gmail.com>
2020-06-05 16:40:56 -0700

    tidy up and test code

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,3 @@
-//User Logic
-$(document).ready(function() {
-  $("form#ProgramLanguage").submit(function(event) {
-    event.preventDefault();
-    const favBeverage =  $("input:radio[name=beverage]:checked").val();
-    const favMusic =  $("input:radio[name=music]:checked").val();
-    const favOutdoor =  $("input:radio[name=outdoor]:checked").val();
-    const favFood =  $("input:radio[name=food]:checked").val();
-    const favAnimal =  $("input:radio[name=animal]:checked").val();
-    let result = myLanguage(favBeverage, favMusic, favOutdoor, favFood, favAnimal);
-  });
-});
-
 $(document).ready(function() {
   $("form#ProgramLanguage").submit(function(event) {
     event.preventDefault();
