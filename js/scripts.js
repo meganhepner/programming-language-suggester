0bc80ca294df016f9be7bde23587c57db483beb3
Megan Hepner <megan.hepner@gmail.com>
2020-06-05 12:14:40 -0700

    combine logic into one larger function

--- js/scripts.js
+++ js/scripts.js
@@ -6,35 +6,20 @@ $(document).ready(function() {
     const favMusic =  $("input:radio[name=music]:checked").val();
     const favOutdoor =  $("input:radio[name=outdoor]:checked").val();
     const favFood =  $("input:radio[name=food]:checked").val();
-    const favAnimal =  $("input:radio[name=animal]:checked").val();
- 
-    let result = yourLanguage(favBeverage, favMusic, favOutdoor, favFood, favAnimal);
+    const favAnimal =  $("input:radio[name=food]:checked").val();
 
-    // if (result = python) {
-    //   $('#python').show();
-    // } else if (result = javascript){
-    //   $('#javascript').show();
-    // } else if (result = COBOL){
-    //   $('#COBOL').show();
-    // } else if (result = COBOL){
-    //   $('#Ruby').show();
-    // }
+    if()
+      if(!!favBeverage === true && !!favMusic === true && !!favOutdoor === true && favFood === "sushi" && !!favAnimal === true){
+        $('#python').show();
+      } else if(!!favBeverage === true && !!favMusic === true && !!favOutdoor === true && favFood === "tartare" && !!favAnimal === true){
+        $('#javascript').show();
+      } else if(!!favBeverage === true && !!favMusic === true && !!favOutdoor === true && favFood === "foie" && !!favAnimal === true){
+        $('#COBOL').show();
+      } else if(!!favBeverage === true && !!favMusic === true && !!favOutdoor === true && favFood === "truffles" && !!favAnimal === true){
+        $('#RUBY').show();
+      }
+  
   });
 });
 
-//Business Logic
-function yourLanguage(favBeverage, favMusic, favOutdoor, favFood, favAnimal) { 
-  if(!!favBeverage === true && !!favMusic === true && !!favOutdoor === true && favFood === "sushi" && !!favAnimal === true){
-    result = ("python");
-  } else if(!!favBeverage === true && !!favMusic === true && !!favOutdoor === true && favFood === "tartare" && !!favAnimal === true){
-    result = "javascript";
-  } else if(!!favBeverage === true && !!favMusic === true && !!favOutdoor === true && favFood === "foie" && !!favAnimal === true){
-    result = "COBOL";
-  } else if(!!favBeverage === true && !!favMusic === true && !!favOutdoor === true && favFood === "truffles" && !!favAnimal === true){
-    result = "Ruby";
-  } 
-  return result;
-  alert(result)
 
- 
-}
