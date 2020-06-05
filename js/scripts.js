e14916fa2e291d85fce6b7f7d57defb8fd4584bb
Megan Hepner <megan.hepner@gmail.com>
2020-06-05 10:58:00 -0700

    collect variable from business logic and use it to display in console.log

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,25 @@ $(document).ready(function() {
     const favFood =  $("input:radio[name=food]:checked").val();
     const favAnimal =  $("input:radio[name=animal]:checked").val();
     let result = yourLanguage(favBeverage, favMusic, favOutdoor, favFood, favAnimal);
-    alert (!!favBeverage)
+
+    if (result === javascript){
+      alert("yay")
+      // ("#python").show();
+    }
+  
   });
 });
 
 //Business Logic
 function yourLanguage(favBeverage, favMusic, favOutdoor, favFood, favAnimal) {
-  if (!!favBeverage === true) {
-    alert ("yay");
-  }
-    // if(!!favBeverage === true && !!favMusic && !!favOutdoor && favFood === "sushi" && !!favAnimal)
- }
+  if(!!favBeverage === true && !!favMusic === true && !!favOutdoor === true && favFood === "sushi" && !!favAnimal === true){
+    result = python;
+  } else if(!!favBeverage === true && !!favMusic === true && !!favOutdoor === true && favFood === "tartare" && !!favAnimal === true){
+    result = javascript;
+  } else if(!!favBeverage === true && !!favMusic === true && !!favOutdoor === true && favFood === "foie" && !!favAnimal === true){
+    result = COBOL;
+  } else if(!!favBeverage === true && !!favMusic === true && !!favOutdoor === true && favFood === "truffles" && !!favAnimal === true){
+    result = Ruby;
+  } 
+  return result;
+}
