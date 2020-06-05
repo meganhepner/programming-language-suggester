51907fda03c462abd4227ad71796a2a5e97b3fe3
Megan Hepner <megan.hepner@gmail.com>
2020-06-05 12:55:17 -0700

    remove form reset button

--- js/scripts.js
+++ js/scripts.js
@@ -8,17 +8,18 @@ $(document).ready(function() {
     const favFood =  $("input:radio[name=food]:checked").val();
     const favAnimal =  $("input:radio[name=food]:checked").val();
 
-    if()
-      if(!!favBeverage === true && !!favMusic === true && !!favOutdoor === true && favFood === "sushi" && !!favAnimal === true){
+    if(!!favBeverage === true && !!favMusic === true && !!favOutdoor === true && favFood === "sushi" && !!favAnimal === true){
         $('#python').show();
-      } else if(!!favBeverage === true && !!favMusic === true && !!favOutdoor === true && favFood === "tartare" && !!favAnimal === true){
+      }else if(!!favBeverage === true && !!favMusic === true && !!favOutdoor === true && favFood === "tartare" && !!favAnimal === true){
         $('#javascript').show();
-      } else if(!!favBeverage === true && !!favMusic === true && !!favOutdoor === true && favFood === "foie" && !!favAnimal === true){
+      }else if(!!favBeverage === true && !!favMusic === true && !!favOutdoor === true && favFood === "foie" && !!favAnimal === true){
         $('#COBOL').show();
-      } else if(!!favBeverage === true && !!favMusic === true && !!favOutdoor === true && favFood === "truffles" && !!favAnimal === true){
-        $('#RUBY').show();
-      }
-  
+      }else if(!!favBeverage === true && !!favMusic === true && !!favOutdoor === true && favFood === "truffles" && !!favAnimal === true){
+      $('#RUBY').show();
+    }
+    
+    
+
   });
 });
 
